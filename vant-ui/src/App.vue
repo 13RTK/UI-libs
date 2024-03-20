88d75eeeec6cab62b86b0e7a6ff98a8b30495449
alex <907785012@qq.com>
2024-03-20 18:48:29 +0800

    Add VantUI

---
+++ vant-ui/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <van-config-provider :theme="currentTheme">
+    <AppLayout
+      :onToggleTheme="toggleTheme"
+      :currentTheme="currentTheme"
+    />
+  </van-config-provider>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import AppLayout from "./components/AppLayout.vue";
+
+const currentTheme = ref("light");
+
+function toggleTheme() {
+  currentTheme.value =
+    currentTheme.value === "light" ? "dark" : "light";
+}
+</script>
---
+++ vant-ui/src/components/AppLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <van-nav-bar
+    title="Title"
+    :left-text="currentThemeIcon"
+    right-text="refresh"
+    @click-right="refresh"
+    @click-left="onToggleTheme"
+  />
+
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o">Tab</van-tabbar-item>
+    <van-tabbar-item icon="search">Tab</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+
+const props = defineProps([
+  "onToggleTheme",
+  "currentTheme",
+]);
+
+const currentThemeIcon = computed(() => {
+  return props.currentTheme === "light" ? "🔆" : "🌙";
+});
+
+const active = ref(0);
+
+function refresh() {
+  window.location.reload();
+}
+</script>
